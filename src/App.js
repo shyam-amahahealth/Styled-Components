e3b8e6f45da4b85fe2f587528f38b83468f48801
shyam-amahahealth <shyam@amahahealth.com>
2023-11-01 19:01:10 +0530

    animation & keyframs in styled components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import "./App.css";
+import { AnimatedLogo } from "./components/Animation";
 import StyledButton, { FancyButton, SubmitButton } from "./components/Button";
-
+import logo from "./logo.svg";
 function App() {
   return (
     <div className="App">
-      <StyledButton type="submit" variant="outline">Styled Button</StyledButton>
+      {/* <img src={logo} className="App-logo" alt="logo" /> */}
+      <AnimatedLogo src={logo} />
+      <StyledButton type="submit" variant="outline">
+        Styled Button
+      </StyledButton>
       <StyledButton>Styled Button</StyledButton>
       <FancyButton as="a">Styled Button</FancyButton>
-      <SubmitButton >Styled Button</SubmitButton>
+      <SubmitButton>Styled Button</SubmitButton>
     </div>
   );
 }
