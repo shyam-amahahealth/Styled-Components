f0ed0601c48cf439b00411ab23d81eab566dd566
shyam-amahahealth <shyam@amahahealth.com>
2023-11-01 18:17:20 +0530

    basic styled component

---
+++ src/components/Button.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+  border: 2px solid #4caf50;
+  background-color: #4caf50;
+  color: #fff;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: medium;
+  cursor: pointer;
+  transition: 0ms.5s all ease-out;
+`;
+export default StyledButton;
