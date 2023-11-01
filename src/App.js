30b57e4957714292794a9e3646b80bc5deace449
shyam-amahahealth <shyam@amahahealth.com>
2023-11-01 18:23:55 +0530

    passing props in styled components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import StyledButton from "./components/Button";
 function App() {
   return (
     <div className="App">
+      <StyledButton variant="outline">Styled Button</StyledButton>
       <StyledButton>Styled Button</StyledButton>
     </div>
   );
