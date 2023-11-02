3e2e4ed6809500a17738dad9abf6c8aafc04826f
shyam-amahahealth <shyam@amahahealth.com>
2023-11-02 07:14:00 +0530

    Added global styles in the component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
-import { ThemeProvider } from "styled-components";
+import { ThemeProvider, createGlobalStyle } from "styled-components";
 import { AnimatedLogo } from "./components/Animation";
-import StyledButton, { DarkButton, FancyButton, SubmitButton } from "./components/Button";
+import StyledButton, {
+  DarkButton,
+  FancyButton,
+  SubmitButton,
+} from "./components/Button";
 import logo from "./logo.svg";
 const theme = {
   dark: {
@@ -12,10 +16,16 @@ const theme = {
     primary: "#fff",
     text: "#000",
   },
+  fontFamily: "Segoe UI",
 };
+const GlobalStyles = createGlobalStyle`
+button{
+  font-family:${(props) => props.theme.fontFamily}
+}`;
 function App() {
   return (
     <ThemeProvider theme={theme}>
+      <GlobalStyles />
       <div className="App">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <AnimatedLogo src={logo} />
